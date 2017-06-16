b615da521a0c37aac24a637b9a25c300b99974ad
yaboisharf <asharfeldden@gmail.com>
2017-06-16 15:00:19 -0400

    message

---
+++ javaScript/script.js
@@ -0,0 +1,39 @@
+/*dropdown menu*/
+function change(x) {
+    x.classList.toggle("change");
+}
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function openMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+/*accordion code*/
+var acc = document.getElementsByClassName("accordion");
+var b;
+
+for (b = 0; b < acc.length; b++) {
+  acc[b].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
+}
